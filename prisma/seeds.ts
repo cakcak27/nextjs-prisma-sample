81830ed199e13cbb3b66ac016b527c7542445ffb
cakcak27 <cakcak27@yahoo.com>
2022-09-14 03:48:26 +0700

    2 files modified, 1 deleted

--- prisma/seeds.ts
+++ prisma/seeds.ts
@@ -1,35 +1,6 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@cakcak27 
-prisma
-/
-prisma-examples
-Public
-Code
-Issues
-57
-Pull requests
-12
-Actions
-Security
-Insights
-prisma-examples/typescript/rest-nextjs-api-routes/prisma/seed.ts /
-@matthewmueller
-matthewmueller fix: try-catch in examples (#3905)
-Latest commit af55318 on Jul 6
- History
- 2 contributors
-@matthewmueller@ruheni
-70 lines (66 sloc)  1.39 KB
+import { PrismaClient, Prisma } from '@prisma/client';
 
-import { PrismaClient, Prisma } from '@prisma/client'
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const userData: Prisma.UserCreateInput[] = [
   {
@@ -75,40 +46,25 @@ const userData: Prisma.UserCreateInput[] = [
       ],
     },
   },
-]
+];
 
 async function main() {
-  console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
-    })
-    console.log(`Created user with id: ${user.id}`)
-  }
-  console.log(`Seeding finished.`)
+  // console.log(`Start seeding ...`);
+  // for (const u of userData) {
+  //   const user = await prisma.user.create({
+  //     data: u,
+  //   });
+  //   console.log(`Created user with id: ${user.id}`);
+  // }
+  // console.log(`Seeding finished.`);
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-You have unread notifications
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
